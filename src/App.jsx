c27eb2f481f012ac80e2d8536f7bd94ec704cea1
Ayush-Khan <ayushkhana230@gmail.com>
2024-05-31 03:57:42 -0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import NavBar from './Header/NavBar'
 import Index from './router/Index'
-
-import Login from './componants/LandingPage'
-// import SimpleForm from './componants/SimpleForm'
-
-
-
 function App() {
 
  
   return (
     <div>
-     <Login/> 
-     
-      {/* <NavBar /> */}
-     {/* <Card/> */}
       <Index />
     </div>
   )
